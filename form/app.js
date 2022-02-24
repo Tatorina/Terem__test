4ab7e6a6635fdebd3359ad906e8026c0d8c63f64
Olga <t_olya@bk.ru>
2022-02-24 16:24:41 +0300

    some changes
    add html layout, ajax

--- form/app.js
+++ form/app.js
@@ -1,126 +1,86 @@
-let container = document.createElement('div')
-container.className = 'container'
-document.querySelector('body').prepend(container)
-const cont = document.querySelector('.container')
+const dropBtn = document.querySelectorAll('.dropbtn')
+const listA = document.querySelectorAll('.list')
 
+let activeBtn = dropBtn[0]
+let activeList = listA[0]
 let feedbackForm = {0: null,  1: null, 2: null, 3: null, 4: null, 'text1': null, 'text2': null}
 
-function createList(j) {
-  let dropDown = document.createElement('div')
-  dropDown.className = 'dropdown' 
-  cont.append(dropDown)
-  const drop = document.querySelectorAll('.dropdown')
-
-  
-  let btn = {
-    tag: 'button',
-    atr:{
-      class:"btn  dropbtn", 
-      type: "button",
-      onclick:'myFunction(event)',
-      id: j
+for (let i = 0; i < dropBtn.length; i++){
+  dropBtn[i].addEventListener('click', function() {
+    if (activeBtn == this){
+      listA[this.id].classList.toggle('show')
+      activeList = listA[this.id]
+    } else {
+      activeList.classList.remove('show')
+      listA[this.id].classList.add('show')
+      activeBtn = this
+      activeList = listA[this.id]
     }
-  }
-  const _btn = document.createElement(btn.tag)
-  for (item in btn.atr){
-    _btn.setAttribute(item, btn.atr[item])
-  }
-  _btn.innerHTML = ''
-
-  drop[j].append(_btn)
-  
-  let list = document.createElement('div')
-  list.className = 'list dropdown__content'
-  list.id = 'myDropdown'
-  drop[j].append(list)
-  const _list = document.querySelectorAll('.list')
-  for (let i = 0; i < 5; i++){
-    let href = document.createElement('a')
-    href.className = 'list__item'
-    href.innerHTML = i + 1
-    href.setAttribute('onclick', 'listClick(event)')
-    href.setAttribute('id', j.toString() + i.toString())
-    _list[j].append(href)
-  }
-  
+    listInd()
+  }) 
 }
 
-function createText() {
-  let container = document.createElement('div')
-  container.className = 'text__container' 
-  cont.append(container)
-  
-  let btn = {
-    tag: 'button',
-    atr:{
-      class:"btn", 
-      type: "button",
-      id: 'send',
-      onclick:'sendBtn(event)'
-    }
-  }
-  const _btn = document.createElement(btn.tag)
-  for (item in btn.atr){
-    _btn.setAttribute(item, btn.atr[item])
-  }
-  _btn.innerHTML = 'Отправить'
-  for(let i = 0; i < 2; i++){
-    let text = document.createElement('textarea')
-    text.className = 'text' 
-    text.setAttribute('id', 'a' + i)
-    container.append(text)
-  }
-  container.append(_btn)
-}
-
-
-
-for (let j = 0; j < 5; j++){
-  createList(j)
-}
-
-createText()
-
-function feedbackArea(){
-  let text = document.createElement('textarea')
-    text.className = 'text text__container' 
-    text.setAttribute('id', 'f__area')
-    container.append(text)
-    cont.append(text)
-}
-
-
-function myFunction(event) {
-  document.getElementById(event.target.id)
-  let list = document.querySelectorAll('.list')
-  list[event.target.id].classList.toggle("show")
-  for(let i = 0; i < 5; i++){
-    if(i != event.target.id){
-      list[i].classList.remove("show")
-    }
-  }
-}
-
-feedbackArea()
-function sendBtn(event){
+document.getElementById('send').onclick = function(){
   feedbackForm['text1'] = document.getElementById('a0').value
   feedbackForm['text2'] = document.getElementById('a1').value
   document.getElementById('a0').value = null
   document.getElementById('a1').value = null
   document.getElementById('f__area').value = JSON.stringify(feedbackForm)
+  send()
   for(key in feedbackForm){
     feedbackForm[key] = null
   }
-  let list = document.querySelectorAll('.list')
   for(let i = 0; i < 5; i++){
-      list[i].classList.remove("show")
-      document.getElementById(i).innerHTML = ''
-  }
+    listA[i].classList.remove("show")
+    document.getElementById(i).innerHTML = ''
+}
 }
 
-function listClick(event){
-  feedbackForm[parseInt(event.target.id[0])] = (parseInt(event.target.id[1]) + 1).toString()
-  document.getElementById(event.target.id[0]).innerHTML = (parseInt(event.target.id[1]) + 1).toString()
-  let list = document.querySelectorAll('.list')
-  list[parseInt(event.target.id[0])].classList.remove("show")
+function listClick(p,i){
+    feedbackForm[i] = p
+    document.getElementsByClassName('dropbtn')[i].innerHTML = p
 }
+
+function listInd() {
+  const EL = document.getElementsByClassName("dropdown__content");
+    for (let i = 0; i < EL.length; i++){
+      EL[i].addEventListener("click", e => {
+        const lang = +e.target.id[1] + 1;
+        listClick(lang, i)
+    });
+    }
+};
+
+function send() {
+  console.log(1)
+  let req = getXmlHttp()
+  req.onreadystatechange = function() {
+    if (req.readyState == 4) {
+      if(req.status == 200) {
+        alert("Ответ сервера: "+req.statusText);
+      } else {
+        alert('error')
+      }
+  }
+}
+  req.open('GET', 'json.php', true); 
+  req.send(null);
+}
+
+function getXmlHttp(){
+  var xmlhttp;
+  try {
+    xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+  } catch (e) {
+    try {
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    } catch (E) {
+      xmlhttp = false;
+    }
+  }
+  if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+    xmlhttp = new XMLHttpRequest();
+  }
+  return xmlhttp;
+}
+
--- layout/app.js
+++ layout/app.js
@@ -1,42 +1,9 @@
 const firstBlock = document.querySelector('.header')
 const cont = document.querySelector('.container')
 
-
-function createModal(){
-  let div = {
-    tag: 'section',
-    atr: {
-      id:'myModal',
-      class:'modal fade',
-      tabindex:'-1',
-      role:'dialog',
-    }
-  }
-  const _modal = document.createElement(div.tag)
-  for (var item in div.atr) {
-    _modal.setAttribute(item, div.atr[item])
-  }
-  _modal.setAttribute('aria-labelledby', 'exampleModalLabel')
-  _modal.setAttribute('aria-hidden', 'true')
-  document.querySelector('.wrapper').prepend(_modal)
-  _modal.insertAdjacentHTML('beforeend', `
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-body">
-        Привет,мир.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-  `)
+window.onclick = function() {
+  document.querySelector('.modal').style.display = 'none'
 }
-$(document).ready(function(){
-  createModal()
-  $('#myModal').modal('show')
-})
-
 
 document.getElementById('hider').onclick = function() {
   firstBlock.classList.toggle('hidden')
