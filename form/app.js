7fdbc2f951d06b877600101a222f99a236616750
Olga <t_olya@bk.ru>
2022-02-25 16:45:35 +0300

    some changes

--- form/app.js
+++ form/app.js
@@ -1,44 +1,45 @@
-const dropBtn = document.querySelectorAll('.dropbtn')
-const listA = document.querySelectorAll('.list')
+const dropBtn = document.querySelectorAll('.dropbtn');
+const listA = document.querySelectorAll('.list');
 
-let activeBtn = dropBtn[0]
-let activeList = listA[0]
-let feedbackForm = {0: null,  1: null, 2: null, 3: null, 4: null, 'text1': null, 'text2': null}
+let activeBtn = dropBtn[0];
+let activeList = listA[0];
+let feedbackForm = {0: null,  1: null, 2: null, 3: null, 4: null, 'text1': null, 'text2': null};
 
 for (let i = 0; i < dropBtn.length; i++){
   dropBtn[i].addEventListener('click', function() {
     if (activeBtn == this){
-      listA[this.id].classList.toggle('show')
-      activeList = listA[this.id]
+      listA[this.id].classList.toggle('show');
+      activeList = listA[this.id];
     } else {
-      activeList.classList.remove('show')
-      listA[this.id].classList.add('show')
-      activeBtn = this
-      activeList = listA[this.id]
+      activeList.classList.remove('show');
+      listA[this.id].classList.add('show');
+      activeBtn = this;
+      activeList = listA[this.id];
     }
-    listInd()
-  }) 
+    listInd();
+  }) ;
 }
 
 document.getElementById('send').onclick = function(){
-  feedbackForm['text1'] = document.getElementById('a0').value
-  feedbackForm['text2'] = document.getElementById('a1').value
-  document.getElementById('a0').value = null
-  document.getElementById('a1').value = null
-  document.getElementById('f__area').value = JSON.stringify(feedbackForm)
-  send()
+  feedbackForm['text1'] = document.getElementById('a0').value;
+  feedbackForm['text2'] = document.getElementById('a1').value;
+  document.getElementById('a0').value = null;
+  document.getElementById('a1').value = null;
+  let jsonAns = JSON.stringify(feedbackForm);
+  document.getElementById('f__area').value = jsonAns;
+  send(jsonAns);
   for(key in feedbackForm){
-    feedbackForm[key] = null
+    feedbackForm[key] = null;
   }
   for(let i = 0; i < 5; i++){
-    listA[i].classList.remove("show")
-    document.getElementById(i).innerHTML = ''
-}
+    listA[i].classList.remove("show");
+    document.getElementById(i).innerHTML = '';
+  }
 }
 
 function listClick(p,i){
-    feedbackForm[i] = p
-    document.getElementsByClassName('dropbtn')[i].innerHTML = p
+    feedbackForm[i] = p;
+    document.getElementsByClassName('dropbtn')[i].innerHTML = p;
 }
 
 function listInd() {
@@ -46,41 +47,24 @@ function listInd() {
     for (let i = 0; i < EL.length; i++){
       EL[i].addEventListener("click", e => {
         const lang = +e.target.id[1] + 1;
-        listClick(lang, i)
+        listClick(lang, i);
     });
     }
 };
 
-function send() {
-  console.log(1)
-  let req = getXmlHttp()
+function send(p) {
+  let req = new XMLHttpRequest();
   req.onreadystatechange = function() {
     if (req.readyState == 4) {
       if(req.status == 200) {
         alert("Ответ сервера: "+req.statusText);
       } else {
-        alert('error')
+        alert('error');
       }
   }
 }
-  req.open('GET', 'json.php', true); 
-  req.send(null);
+  req.open('GET', 'serv.php', true); 
+  req.send(p);
 }
 
-function getXmlHttp(){
-  var xmlhttp;
-  try {
-    xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
-  } catch (e) {
-    try {
-      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    } catch (E) {
-      xmlhttp = false;
-    }
-  }
-  if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
-    xmlhttp = new XMLHttpRequest();
-  }
-  return xmlhttp;
-}
 
--- layout/app.js
+++ layout/app.js
@@ -1,20 +1,20 @@
-const firstBlock = document.querySelector('.header')
-const cont = document.querySelector('.container')
+const firstBlock = document.querySelector('.header');
+const cont = document.querySelector('.container');
 
 window.onclick = function() {
-  document.querySelector('.modal').style.display = 'none'
+  document.querySelector('.modal').style.display = 'none';
 }
 
 document.getElementById('hider').onclick = function() {
-  firstBlock.classList.toggle('hidden')
+  firstBlock.classList.toggle('hidden');
 }
 
 document.getElementById('swap').onclick = function() {
-  cont.classList.toggle('_swap')
+  cont.classList.toggle('_swap');
     if (getComputedStyle(cont).display == 'block') {
-      let mainList = document.querySelectorAll('.main')
-      mainList[1].remove()
-      cont.prepend(mainList[1])
+      let mainList = document.querySelectorAll('.main');
+      mainList[1].remove();
+      cont.prepend(mainList[1]);
     }
 }
 
